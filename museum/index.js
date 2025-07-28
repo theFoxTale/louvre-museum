211f31cc050a7d13e63c59120c7f62fd692ee304
Annie <makarenkoanna@yandex.ru>
2025-07-28 10:36:27 +0300

    style: make welcome adaptive

--- museum/index.js
+++ museum/index.js
@@ -11,3 +11,15 @@ volumeProgress.addEventListener('input', function() {
     const value = this.value;
     this.style.background = `linear-gradient(to right, var(--dark-red) 0%, var(--dark-red) ${value}%, var(--slider-grey) ${value}%, var(--slider-grey) 100%)`
 })
+
+let adaptiveMenuShown = false;
+function toggleAdaptiveMenu() {
+    const menuIcon = document.querySelector('.menu-icon');
+    menuIcon.classList.toggle('menu-open');
+
+    const welcomeText = document.querySelector('.welcome-text');
+    welcomeText.classList.toggle('hidden-by-menu');
+
+    const navigation = document.querySelector('.header-navigation-list');
+    navigation.classList.toggle('header-navigation-list-on-left');
+}
