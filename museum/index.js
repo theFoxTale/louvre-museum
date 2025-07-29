c49a645ec224ba0deb62d03f93c037cf9a2b8a59
Annie <makarenkoanna@yandex.ru>
2025-07-29 08:39:08 +0300

    style: fix css style and work

--- museum/index.js
+++ museum/index.js
@@ -1,13 +1,5 @@
-const playProgress = document.querySelector('.play-progress');
 
-playProgress.addEventListener('input', function() {
-    const value = this.value;
-    this.style.background = `linear-gradient(to right, var(--dark-red) 0%, var(--dark-red) ${value}%, var(--slider-grey) ${value}%, var(--slider-grey) 100%)`
-})
-
-const volumeProgress = document.querySelector('.volume-progress');
-
-volumeProgress.addEventListener('input', function() {
-    const value = this.value;
-    this.style.background = `linear-gradient(to right, var(--dark-red) 0%, var(--dark-red) ${value}%, var(--slider-grey) ${value}%, var(--slider-grey) 100%)`
-})
+function clickSlider(slider) {
+    const value = slider.value;
+    slider.style.background = `linear-gradient(to right, var(--dark-red) 0%, var(--dark-red) ${value}%, var(--slider-grey) ${value}%, var(--slider-grey) 100%)`;
+}
